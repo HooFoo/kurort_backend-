fe934c1015c4f4e6e55ab0988c97f4f0977e802f
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-03-23 00:39:49 +0700

    Client files

---
+++ client/src/components/SearchBox.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.row
+  .col.s12.search-wrapper( flex=true layout='row' )
+    i.material-icons search
+    input.search-box( name='searchField' )
+</template>
+
+<script>
+    export default {
+        name: 'search-box'
+    }
+</script>
+
+<style lang="scss">
+.search-wrapper {
+  position: relative;
+
+  input.search-box {
+    color: white;
+    padding-left: 26px;
+    width: 250px;
+
+    &:focus {
+      border-color: rgba(255, 255, 255, 0.87);
+      box-shadow: 0 1px 0 0 rgba(255, 255, 255, 0.87);
+    }
+  }
+
+  i.material-icons {
+    color: white;
+    position: absolute;
+    top: 11px;
+  }
+}
+</style>
---
+++ client/src/components/UserLogin.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+form.user-form( novalidate=true )
+  .input-field
+    label(for='email') Email
+    input.user-form-input( name='email' type='text' )
+  .input-field
+    label(for='password') Password
+    input.user-form-input( name='password' type='password' )
+  input.btn( type='submit' value='Register' )
+</template>
+
+<script>
+    export default {
+        name: 'user-login'
+    }
+</script>
