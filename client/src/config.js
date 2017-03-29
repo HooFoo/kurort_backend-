6d3deca3bf90ff3e17f5a736bcd40c8ae96b71b3
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-03-29 15:57:43 +0700

    Updated client

--- client/src/config.js
+++ client/src/config.js
@@ -14,5 +14,6 @@ Vue.use(VueResource)
 Vue.http.options.root = API_URL;
 Vue.http.options.xhr = { withCredentials: true };
 Vue.http.options.emulateJSON = true;
+console.log(GMAP_KEY);
 
 Vue.config.productionTip = false;
