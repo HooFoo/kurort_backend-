1b0c5f560077ea93743cb84036dac92276ecb378
Dmitrii Davydov <swember@gmail.com>
2017-04-30 00:52:10 +0300

    fetch points and print markers on map

--- client/src/components/g_map.vue
+++ client/src/components/g_map.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
   .flex.layout-column
     #map
-      gmap-map( ':center'="center" ':zoom'="zoom")
+      gmap-map( ':center'="center" ':zoom'="zoom" '@idle'="updatePoints" 'ref'="map" )
+        template( 'v-for'="point in points" )
+          map-marker( ':point'= "point" )
 </template>
 <script>
+  import _ from 'lodash'
+  import MapMarker from './map_marker'
+
   export default {
+    components: {
+      'map-marker': MapMarker
+    },
     data () {
       return {
         center: { lat: 55.75370903771494, lng: 37.61981338262558 },
-        zoom: 13
+        zoom: 13,
+        points: []
       }
     },
     created () {
@@ -17,6 +26,38 @@
           this.center = { lat: position.coords.latitude, lng: position.coords.longitude }
         })
         return this.center
+      }
+    },
+    methods: {
+      updatePoints () {
+        let bounds = this.$refs.map.$mapObject.getBounds()
+        if (bounds) {
+          let parsed = {
+            sw: {
+              lat: bounds.getSouthWest().lat(),
+              lng: bounds.getSouthWest().lng()
+            },
+            ne: {
+              lat: bounds.getNorthEast().lat(),
+              lng: bounds.getNorthEast().lng()
+            }
+          }
+          this.$api.points.forRect({}, { rect: parsed })
+            .then(response => {
+              this.points = []
+              _.each(_.values(response.body.points), point => {
+                let mapped = {
+                  id: point.id,
+                  position: {
+                    lat: point.attributes.lat,
+                    lng: point.attributes.lng
+                  },
+                  icon: point.attributes.pointType.icon.url
+                }
+                this.points.push(mapped)
+              })
+            }).catch(response => { console.log(response) })
+        }
       }
     }
   }
---
+++ client/src/components/map_marker.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  gmap-marker( ':position'="point.position" ':icon'="point.icon" '@click'="onClick" )
+</template>
+<script>
+  export default {
+    name: 'map-marker',
+    props: ['point'],
+    methods: {
+      onClick () {
+        console.log('cliiiiick')
+      }
+    }
+  }
+</script>
+<style>
+</style>
